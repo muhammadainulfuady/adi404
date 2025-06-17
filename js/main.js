3a5bd97a126733632f4b6c83d160026fd9814cee
= <=>
2025-06-17 20:22:05 +0700

    portfolio pribadi

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const pertambahan = function (a, b) {
+  return a + b;
+};
+
+const perkalian = function (a, b) {
+  return a * b;
+};
+
+const angka1 = 10;
+const angka2 = 10;
+const result = (perkalian(pertambahan(angka1, angka2), 2));
+
+console.log(result);
